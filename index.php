829ed9444f25d02ce454def8cc91f2b8d8462bac
oluyemi-007 <abimboluyemi@gmail.com>
2024-03-21 23:53:43 +0100

    Little changes in index

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 require 'app/autoload.php';
 
-echo provider::check('070935100865');
+echo provider::check('08056014725');
 
-echo provider::get('nigeria', '070935100865');
+echo provider::get('nigeria', '07026113832');
 
 
 
